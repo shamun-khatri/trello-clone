badd0cfff6fbb60890ef3d11f391b9b2741bd12b
shamun-khatri <shamunkhatri@gmail.com>
2023-07-04 22:05:12 +0530

    Zustand Installed

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,6 +1,21 @@
+'use client'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+
 function Board() {
+    useEffect(() => {
+        //get board
+    }, [])
+
   return (
-    <div>Board</div>
+    <DragDropContext>
+        <Droppable droppableId='board' direction='horizontal' type='column' >
+            {(provided) => (
+                <div>
+                    {/* rendering all the columns */} 
+                </div>
+            )}
+        </Droppable>
+    </DragDropContext>
   )
 }
 
